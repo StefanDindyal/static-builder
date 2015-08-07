f2ee91879a1e5b2cf841d9ad14cbe4a313e31169
StefanDindyal <stefan@rgnrtr.com>
2015-08-07 15:20:09 -0400

    Practicing OO Js

--- app.js
+++ app.js
@@ -1,3 +1,65 @@
 "use strict";
 
-var mai = new Search(indexJSON, null);
+// var mai = new Search(json, null);
+
+// var htmlString = "";
+
+// for (var i = 0; i < indexJSON.story.length; i++) {	
+// 	htmlString += "<h1>";
+// 	htmlString += indexJSON.story[i].name;
+
+// 		if (indexJSON.story[i].synopsis !== null){
+// 			htmlString += "<br><strong><p>";
+// 			htmlString += indexJSON.story[i].synopsis;
+// 			htmlString += "</p></strong>";
+// 		}
+
+// 		if (indexJSON.story[i].characters !== null) {			
+
+// 			for (var j = 0; j < indexJSON.story[i].characters.length; j++) {
+// 				console.log(indexJSON.story[i].characters.length);
+// 				htmlString += "<h2>";
+// 				htmlString += indexJSON.story[i].characters[j].name;
+// 				htmlString += "</h2>";
+// 			}
+
+// 		}
+
+// 	htmlString += "</h1>";
+// }
+
+// "Eleanor": {
+			
+		// 	"characters": {
+
+		// 		"Eleanor Blake": {
+		// 			"age": 13,
+		// 			"gender": "Female",
+		// 			"eyeColor": "Green",
+		// 			"hairColor": "Purple",
+		// 			"race": "Human/Demon Hybrid",
+		// 			"affiliation": "Unknown",
+		// 			"occupation": "Student (Polydor Academy)",
+		// 			"classLevel": "C",
+		// 			"threatLevel": "White",
+		// 			"eval": "A young, shy girl with issues coping with her own existence. She wonders where her place in the world is and why many people don't seem to accept her. Though she has others that are close to her she sees them as precious gifts that she thinks she is undeserving of. Unknown to them she is more greatful to them because she counts herself lucky to know them. She would rather sacrifice herself and her happiness if it means sparing them any pain or agony."
+		// 		},
+		// 		"Cheryl Jacoby": null,
+		// 		"Adrian Moore": null,
+		// 		"Gerard Allen": null,
+		// 		"Claire Shaw": null,
+		// 		"Helena Blake": null
+
+		// 	}
+
+		// }
+
+		// var test = new list(json, elem);
+
+// (function(){
+// 	function foo(){
+// 		console.log('foobar');
+// 	}
+
+// 	foo();
+// }());
--- index.js
+++ index.js
@@ -1,92 +1,116 @@
-"use strict";
+(function(){
 
-// "Eleanor": {
-			
-		// 	"characters": {
+	"use strict";
 
-		// 		"Eleanor Blake": {
-		// 			"age": 13,
-		// 			"gender": "Female",
-		// 			"eyeColor": "Green",
-		// 			"hairColor": "Purple",
-		// 			"race": "Human/Demon Hybrid",
-		// 			"affiliation": "Unknown",
-		// 			"occupation": "Student (Polydor Academy)",
-		// 			"classLevel": "C",
-		// 			"threatLevel": "White",
-		// 			"eval": "A young, shy girl with issues coping with her own existence. She wonders where her place in the world is and why many people don't seem to accept her. Though she has others that are close to her she sees them as precious gifts that she thinks she is undeserving of. Unknown to them she is more greatful to them because she counts herself lucky to know them. She would rather sacrifice herself and her happiness if it means sparing them any pain or agony."
-		// 		},
-		// 		"Cheryl Jacoby": null,
-		// 		"Adrian Moore": null,
-		// 		"Gerard Allen": null,
-		// 		"Claire Shaw": null,
-		// 		"Helena Blake": null
+	var json = {
 
-		// 	}
+		"story": [
 
-		// }
-
-var indexJSON = 
-{
-
-	"story": [
-
-		{
-			"name": "Wings of Destiny",
-			"synopsis": "Things start to happen here.",
-			"characters": [
-				{
-					"name": "Ivran Price"
-				}
-			]
-		},
-		{
-			"name": "Eternal Wanderer",
-			"synopsis": null,
-			"characters": null
-		},
-		{
-			"name": "Eleanor",
-			"synopsis": null,
-			"characters": [
-				{
-					"name": "Eleanor Blake"
-				},
-				{
-					"name": "Cheryl Jacoby"
-				}
-			]
-		}
-
-	]
-	
-};
-
-var elem = document.getElementById('mai');
-
-var htmlString = "";
-for (var i = 0; i < indexJSON.story.length; i++) {	
-	htmlString += "<h1>";
-	htmlString += indexJSON.story[i].name;
-
-		if (indexJSON.story[i].synopsis !== null){
-			htmlString += "<br><strong><p>";
-			htmlString += indexJSON.story[i].synopsis;
-			htmlString += "</p></strong>";
-		}
-
-		if (indexJSON.story[i].characters !== null) {			
-
-			for (var j = 0; j < indexJSON.story[i].characters.length; j++) {
-				console.log(indexJSON.story[i].characters.length);
-				htmlString += "<h2>";
-				htmlString += indexJSON.story[i].characters[j].name;
-				htmlString += "</h2>";
+			{
+				"name": "Wings of Destiny",
+				"synopsis": "Things start to happen here.",
+				"characters": 
+				[
+					{
+						"name": "Ivran Price"
+					}
+				],
+				"worlds": 
+				[
+					{"name": "Earth"}
+				]
+			},
+			{
+				"name": "Eternal Wanderer",
+				"synopsis": null,
+				"characters": null,
+				"world": null
+			},
+			{
+				"name": "Eleanor",
+				"synopsis": null,
+				"characters": 
+				[
+					{
+						"name": "Eleanor Blake",
+						"age": 13,
+						"gender": "Female",
+						"eyeColor": "Green",
+						"hairColor": "Purple",
+						"race": "Human/Demon Hybrid",
+						"affiliation": "Unknown",
+						"occupation": "Student (Polydor Academy)",
+						"classLevel": "C",
+						"threatLevel": "White",
+						"eval": "A young, shy girl with issues coping with her own existence. She wonders where her place in the world is and why many people don't seem to accept her. Though she has others that are close to her she sees them as precious gifts that she thinks she is undeserving of. Unknown to them she is more greatful to them because she counts herself lucky to know them. She would rather sacrifice herself and her happiness if it means sparing them any pain or agony.",
+						"alt": 
+						[
+							{"name": "Dark Lord Eleanor"}
+						]
+					},
+					{
+						"name": "Cheryl Jacoby"
+					}
+				],
+				"worlds": 
+				[
+					{"name": "Earth"},
+					{"name": "Asheron"},
+					{"name": "Requiem"}
+				]
 			}
 
+		]
+		
+	};
+
+	var elem = document.getElementById('mai');
+
+	function list(data, element)
+	{
+		this.info = data;
+		this.element = element;
+		this.output = null;
+	}
+
+	list.prototype.sort = function()
+	{
+		var htmlString = "";
+		for (var i = 0; i < this.info.story.length; i++) 
+		{	
+			htmlString += "<div>";
+			htmlString += json.story[i].name;
+
+				// if (indexJSON.story[i].synopsis !== null){
+				// 	htmlString += "<br><strong><p>";
+				// 	htmlString += indexJSON.story[i].synopsis;
+				// 	htmlString += "</p></strong>";
+				// }
+
+				// if (indexJSON.story[i].characters !== null) {			
+
+				// 	for (var j = 0; j < indexJSON.story[i].characters.length; j++) {
+				// 		console.log(indexJSON.story[i].characters.length);
+				// 		htmlString += "<h2>";
+				// 		htmlString += indexJSON.story[i].characters[j].name;
+				// 		htmlString += "</h2>";
+				// 	}
+
+				// }
+			htmlString += "</div>";
 		}
+		return this.output = htmlString;
+	}
 
-	htmlString += "</h1>";
-}
+	list.prototype.print = function()
+	{
+		this.element.innerHTML = this.output;
+		return true;
+	}
 
-elem.innerHTML = htmlString;
+	var test = new list(json, elem);
+
+	test.sort();
+	test.print();
+
+}());
