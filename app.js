127f395baf480330991c4fa1181a8296d59cc654
Stefan Dindyal <edgeless@Edgeless.home>
2015-08-22 22:57:22 -0400

    added some logic

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-function log () {
+// function log () {
 
-	var args = Array.prototype.slice.call(arguments);
-	console.log.apply(console, args);	
+// 	var args = Array.prototype.slice.call(arguments);
+// 	console.log.apply(console, args);
+// 	return false;
 	// return args;
 
-}
+// }
 
 // var my_object = {
 //     '0': 'zero',
@@ -18,6 +19,30 @@ function log () {
 
 // console.log(sliced);
 
-var a = [1,{"one":1},2,3,4,5,6];
+// var a = [1,{"one":1},2,3,4,5,6];
 
 // console.log(a);
+
+// function app(){
+// 	return "Hello World!";
+// }
+
+function App(){
+	this.list = {};
+}
+
+App.prototype.put = function(key, value){
+	this.list[key] = value;
+}
+
+App.prototype.show = function(){
+	return this.list;
+}
+
+var app = new App();
+
+app.put("person","stefan");
+app.put("age",12);
+app.put(12,1234);
+
+console.log(app.show());
